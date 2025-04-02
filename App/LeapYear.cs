312673c37c45207768173aff7d89b46ec6cc91fd
Окладников Фёдор Андреевич <f.okladnikov@skbkontur.ru>
2025-04-02 17:40:53 +0700

    fix half of the tests

--- App/LeapYear.cs
+++ App/LeapYear.cs
@@ -5,6 +5,6 @@ public static class LeapYear
     public static bool IsLeapYear(int year) 
     {
         //TODO напишите тут свое решение
-        throw new NotImplementedException();
+        return false;
     }
 }
